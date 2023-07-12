35039df5caa107dfa82e80bde7e801e73916206b
Russ <qatsby@yandex.com>
2023-07-12 18:52:03 -0400

    04

--- 1.JavaSyntax/src/com/javarush/task/task01/task0109/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0109/Solution.java
@@ -7,6 +7,6 @@ package com.javarush.task.task01.task0109;
 public class Solution {
     public static void main(String[] args) {
         int a= 5;
-        int b = 6;//напишите тут ваш код
+        int b = 6; //напишите тут ваш код
     }
 }
