2c14ffcc38bc9a76cc6ada85566baf81900f9fc1
Russ <qatsby@yandex.com>
2023-07-12 19:09:18 -0400

    05

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0103/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task01.task0103;
+
+/*
+Мой юный друг
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int currentDate = 3126;
+        int birthday = currentDate - 8;
+        System.out.println(birthday);//напишите тут ваш код
+    }
+}
