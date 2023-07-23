82aec91bdbca4fd8b1b1c006b2fa9b5c60f6af10
Russ <qatsby@yandex.com>
2023-07-22 22:40:22 -0400

    .

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0206/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task02.task0206;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        String word = "Hello";
+        String phrase = "The";
+        String line = "Beautiful";
+        String text = "World";
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0214/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.pro.task02.task0214;
+
+import java.util.Scanner;
+
+/* 
+Чтение и преобразование строк
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String str3 = scanner.nextLine();
+        String str2 = scanner.nextLine();
+        String str1 = scanner.nextLine();
+
+        System.out.println(str1);
+        System.out.println(str2.toUpperCase());
+        System.out.println(str3.toLowerCase());
+        //напишите тут ваш код
+
+    }
+}
