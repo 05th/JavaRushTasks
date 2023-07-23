4da4b61f7e4358097f679afbe134de7af31b101c
Russ <qatsby@yandex.com>
2023-07-23 13:38:53 -0400

    .

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0215/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task02.task0215;
+
+import java.util.Scanner;
+
+/* 
+Чтение чисел
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int result = (a + b + c) / 3;//напишите тут ваш код
+        System.out.println(result);
+    }
+}
