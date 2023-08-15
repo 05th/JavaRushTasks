2104956bb1ccf1b05eee27453a06c6b1888138ed
Russ <qatsby@yandex.com>
2023-08-14 22:41:20 -0400

    .

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,54 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod() {
+
+    }
+
+    public static void universalMethod(String str) {
+
+    }
+
+    public static void universalMethod(int x) {
+
+    }
+
+    public static void universalMethod(String x, int y) {
+
+    }
+
+    public static void universalMethod(int[] array) {
+
+    }
+
+    public static void universalMethod(int[][] array) {
+
+    }
+
+    public static void universalMethod(String[] str) {
+
+    }
+
+    public static void universalMethod(String[][] str2) {
+
+    }
+
+    public static void universalMethod(double x) {
+
+    }
+
+    public static void universalMethod(float y) {
+
+    }
+
+    //напишите тут ваш код
+}
