43f2d183c2593cdcf8fb0ad4f0bbb81c27400d71
Russ <qatsby@yandex.com>
2023-08-24 21:43:58 -0400

    .

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0702/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task07.task0702;
+
+/* 
+Максимальное целое
+*/
+
+public class Solution {
+    //напишите тут ваш код
+    byte b = 127;
+    short s = 32767;
+    int i = 2147483647;
+    long l = 9223372036854775807l;
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0703/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task07.task0703;
+
+/* 
+Минимальное целое
+*/
+
+public class Solution {
+    //напишите тут ваш код
+    byte b = -128;
+    short s = -32768;
+    int i = -2147483648;
+    long l = -9223372036854775808l;
+}
