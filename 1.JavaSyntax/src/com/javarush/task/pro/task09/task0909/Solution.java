55bd564945ed02f4fe760e4ab513d5017c33b667
Russ <qatsby@yandex.com>
2023-09-19 22:01:11 -0400

    .

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0909/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task09.task0909;
+
+/* 
+Экранирование символов
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        String str1 = "It's a Windows path: \"C:\\Program Files\\Java\\jdk-13.0.0\\bin\"";
+        System.out.println(str1);
+        String str2 = "It's a Java string: \\\"C:\\\\Program Files\\\\Java\\\\jdk-13.0.0\\\\bin\\\"";
+        System.out.println(str2);
+
+    }
+}
